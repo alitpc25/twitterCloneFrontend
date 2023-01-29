1ef3ce3f3886194295040ed2673be6e4da2fa326
alitpc25 <alitpc2525@gmail.com>
2023-01-29 23:11:06 +0300

    button functionality update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
 import Login from './pages/Login';
 import Register from './pages/Register';
 import React from 'react';
-import { useAppSelector } from './hooks';
+import { useAppSelector } from './redux/hooks';
 import GlobalRightColBox from './components/GlobalRightColBox';
 import GlobalLeftColBox from './components/GlobalLeftColBox';
 import { BsTwitter } from 'react-icons/bs'
@@ -17,34 +17,30 @@ import { IoMdNotificationsOutline } from "react-icons/io"
 import { MdOutlineMail } from "react-icons/md"
 import { BsBookmarks} from "react-icons/bs"
 import { RiFileListLine } from "react-icons/ri"
-import { CiCircleMore } from "react-icons/ci"
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi"
 import { FiSettings } from "react-icons/fi"
 import HomeInnerPage from './pages/HomeInnerPage';
 import ProfileInnerPage from './pages/ProfileInnerPage';
+import MoreDropdownButton from './components/MoreDropdownButton';
 
 function App() {
 
   const user = useAppSelector(state => state.user)
-  /*
-    const dispatch = useAppDispatch()
-
-    useEffect(() => {
-        
-        axios.get(`/api/v1/users`, { headers: {"Authorization" : `Bearer ${user.jwtToken}`} })
-        .then(res => {
-          setUsers(res.data);
-        }).catch(err => {
-            console.log(err)
-            let userState = {
-                username: null,
-                jwtToken: null,
-                loggedIn: false
-            }
-            dispatch(updateUserInfo(userState))
-        })
-      }, []);
-      */
-
+  
+  if(!user.loggedIn) {
+    return (
+      <div className="App">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<Login />} />
+              </Routes>
+            </BrowserRouter>
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
       <div className="flex flex-row divide-x">
@@ -57,17 +53,21 @@ function App() {
                 <GlobalLeftColBox icon={BsBookmarks} text="Bookmarks" routeTo="/bookmarks"></GlobalLeftColBox>
                 <GlobalLeftColBox icon={RiFileListLine} text="Lists" routeTo="/lists"></GlobalLeftColBox>
                 <GlobalLeftColBox icon={FiSettings} text="Settings" routeTo="/settings"></GlobalLeftColBox>
-                <GlobalLeftColBox icon={CiCircleMore} text="More" routeTo="/more"></GlobalLeftColBox>
-                <GlobalLeftColBox icon={FaFeatherAlt} text="Tweet" routeTo="/tweet"></GlobalLeftColBox>
+                
+                <MoreDropdownButton>
+                  <GlobalLeftColBox icon={HiOutlineDotsCircleHorizontal} text="More"></GlobalLeftColBox>
+                </MoreDropdownButton>
+
+                <GlobalLeftColBox icon={FaFeatherAlt} text="Tweet"></GlobalLeftColBox>
+
                 <GlobalLeftColBox icon={FaRegUserCircle} text={user.username} routeTo="/profile"></GlobalLeftColBox>
             </div>
             <div id="feed" className="basis-2/4">
             <BrowserRouter>
               <Routes>
-                <Route path="/" element={user.loggedIn ? <HomeInnerPage /> : <Login />} />
-                <Route path="/login" element={user.loggedIn ? <HomeInnerPage /> : <Login />} />
-                <Route path="/register" element={user.loggedIn ? <HomeInnerPage /> : <Register />} />
-                <Route path="/profile" element={user.loggedIn ? <ProfileInnerPage user={user} /> : <Login />} />
+                <Route path="/" element={<HomeInnerPage />} />
+                <Route path="/profile" element={<ProfileInnerPage user={user} />} />
+                <Route path="*" element={<HomeInnerPage />} />
               </Routes>
             </BrowserRouter>
             </div>
